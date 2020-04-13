067d3b730e5adac13375af56f23fdc49c160ed25
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2020-04-13 22:56:06 +0300

    v.0.3.1

--- app/src/main/java/com/snorri/travelguide/LocalLoginActivity.kt
+++ app/src/main/java/com/snorri/travelguide/LocalLoginActivity.kt
@@ -1,12 +1,22 @@
 package com.snorri.travelguide
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LocalLoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_login)
+
+        // Back to the MainActivity
+        val btnCancelLocal = findViewById<Button>(R.id.btnCancelLocal)
+        btnCancelLocal.setOnClickListener {
+            val intentbtnCancelLocal = Intent(this, MainActivity::class.java)
+            startActivity(intentbtnCancelLocal)
+        }
+
     }
 }
